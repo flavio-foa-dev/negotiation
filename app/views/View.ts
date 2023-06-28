e69e80924f5c70d5c014bef9a2e0ff55b1383830
Flavio Andrade <foa12381@gmail.com>
2023-06-28 12:24:57 -0300

    feat/validation/dates module2

--- app/views/View.ts
+++ app/views/View.ts
@@ -2,14 +2,25 @@ export abstract class View<T> {
 
   protected element: HTMLElement;
 
-  constructor(selector: string){
-    this.element = document.querySelector(selector);
+  constructor(selector: string) {
+    const element: HTMLElement | null = document.querySelector(selector);
+    if (element) {
+      this.element = element;
+    } else {
+      throw new Error('Cannot find element with selector');
+    }
   }
 
   protected abstract template(model: T, propsClass?:string): string
 
   update(model: T, propsClass?:string): void {
+    const tempStart = performance.now();
     const template = this.template(model, propsClass);
     this.element.innerHTML = template;
+    const tempEnd = performance.now();
+
+    console.log(`Tempod de execucao update ${(tempEnd - tempStart)/100} secundos`);
+
+
   }
 }
