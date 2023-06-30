413a4b200187e37fcb48f30e167b90dc4e949d68
Flavio Andrade <foa12381@gmail.com>
2023-06-30 08:06:06 -0300

    decorators

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,7 +2,7 @@ import { Negotiation } from './negociacao';
 
 export class Negotiations {
   private negotiations: Negotiation[] = [];
-  private negotiationss: Array<Negotiation> = [];
+  //private negotiationss: Array<Negotiation> = [];
 
   adiciona(negotiation: Negotiation ): void {
     this.negotiations.push(negotiation);
